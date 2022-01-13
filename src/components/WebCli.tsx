99d9d5845e4a59ef7d0a433f49d5d45414354522
Ant <ryu4979@gmail.com>
2022-01-13 15:14:00 +0900

    fix: CLI画面にfocus処理を実装

--- src/components/WebCli.tsx
+++ src/components/WebCli.tsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "css/cli.css";
 
+type CliComponentState = {
+  cliInput: React.RefObject<HTMLInputElement>
+}
+
 const WebCli: React.FC = () => {
+  const [cliComponentState] = useState<CliComponentState>({
+    cliInput: React.createRef()
+  })
+
+  const focus = () => {
+    cliComponentState.cliInput.current?.focus();
+  }
+
   return (
-    <div className="cli">
+    <div className="cli" onClick={focus}>
       <pre>
 {`
-YaitoCLI 1.1.2
+YaitoCLI風 1.1.2
 
   Usage: yaito <command>
 
-  Examples: yaito --bio
+  Examples: yaito --help
 `}
       </pre>
 
       <form action="">
-        <span>$</span>
-        <input type="text" className="cli-input" />
+        <span>$ </span>
+        <input
+          type="text"
+          className="cli-input"
+          ref={cliComponentState.cliInput}
+        />
       </form>
     </div>
   );
